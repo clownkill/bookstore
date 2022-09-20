aadf5e029e63a24ebb7834990da6a40c0c0d05b1
Evgeniy Alehnovich <evgeniy_alehnovich@proton.me>
2022-09-20 11:28:26 +0300

    add book cover

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 import uuid
 from django.contrib.auth import get_user_model
 from django.db import models
@@ -13,6 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,3 +12,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
